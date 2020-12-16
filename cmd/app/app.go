ba8697a700e0e6433bdb86e53b942adae598e3ed
Andrey Lozovoy <lozovoya@gmail.com>
2020-12-16 12:28:44 +0300

    dto

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -1,10 +1,21 @@
 package app
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/lozovoya/agohomework1.git/cmd/app/dto"
+	"log"
 	"net/http"
+)
+
+var (
+	ErrEmptyLogin    = errors.New("Login is empty")
+	ErrEmptyPassword = errors.New("Password is empty")
+	ErrParse         = errors.New("Body parse error")
 )
 
 type Server struct {
@@ -17,10 +28,20 @@ func NewServer(mux chi.Router, pool *pgxpool.Pool) *Server {
 }
 
 func (s *Server) Init() error {
-	s.mux.With(middleware.Logger).Get("/api/users", s.AddUser)
+	s.mux.With(middleware.Logger).Put("/api/users", s.AddUser)
 	return nil
 }
 
 func (s *Server) AddUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("ok"))
+
+	var user *dto.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		log.Println(ErrParse)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(user)
+	return
 }
---
+++ cmd/app/dto/dto.go
@@ -0,0 +1,6 @@
+package dto
+
+type User struct {
+	login    string `json:"login"`
+	password string `json:"password"`
+}
